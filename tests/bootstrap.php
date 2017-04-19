efac0517154659b02594e7d39c9ce7e104b4cd37
Alexander Makarov <sam@rmcreative.ru>
2017-04-19 17:27:56 +0300

    Fix for phpunit 6 compatibility

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -13,3 +13,5 @@ require_once(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
 Yii::setAlias('@yiiunit/extensions/twig', __DIR__);
 Yii::setAlias('@yii/twig', dirname(__DIR__));
+
+require_once(__DIR__ . '/compatibility.php');
