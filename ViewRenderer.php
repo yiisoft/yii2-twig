3193d4b28c072f2e00443c6204dfcde21b638b8b
Andrzej Broński <andrzej1_1@o2.pl>
2017-07-29 13:10:37 +0200

    Move setting lexer options to render function

--- ViewRenderer.php
+++ ViewRenderer.php
@@ -145,11 +145,6 @@ class ViewRenderer extends BaseViewRenderer
         }
 
         $this->twig->addGlobal('app', \Yii::$app);
-
-        // Change lexer syntax (must be set after other settings)
-        if (!empty($this->lexerOptions)) {
-            $this->setLexerOptions($this->lexerOptions);
-        }
     }
 
     /**
@@ -171,8 +166,14 @@ class ViewRenderer extends BaseViewRenderer
         if ($view instanceof View) {
             $this->addFallbackPaths($loader, $view->theme);
         }
+
         $this->addAliases($loader, Yii::$aliases);
         $this->twig->setLoader($loader);
+
+        // Change lexer syntax (must be set after other settings)
+        if (!empty($this->lexerOptions)) {
+            $this->setLexerOptions($this->lexerOptions);
+        }
 
         return $this->twig->render(pathinfo($file, PATHINFO_BASENAME), $params);
     }
