22f40b9c58ff4aeaafef0799661cc2d2eda1d364
Alexander Makarov <sam@rmcreative.ru>
2020-10-31 01:15:52 +0300

    Add compatibility fix to phpunit

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,3 +12,5 @@ require_once(__DIR__ . '/../vendor/autoload.php');
 require_once(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
 Yii::setAlias('@yiiunit/twig', __DIR__);
+
+require_once(__DIR__ . '/compatibility.php');
